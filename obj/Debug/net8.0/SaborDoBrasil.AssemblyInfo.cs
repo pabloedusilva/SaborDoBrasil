8f0f20f0f05962071607eea9a0de6336dc68520d
Pablo <pablo.silva.edu@gmail.com>
2025-05-06 10:45:40 -0300

    voltei o site para a versão Beta

--- obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
+++ obj/Debug/net8.0/SaborDoBrasil.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9feb2ec4daa8c55c3182afdf3960978093676492")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+946f2324edf27c3b53405dd8f1314a5cd6257e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaborDoBrasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
